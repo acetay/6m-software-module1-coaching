e85eacf1b8498e569bc9d9856df3d6c9f1ded9c0
ace tay <aceveron@gmail.com>
2022-12-04 19:56:25 +0800

    questions done

--- src/higher-order-func.js
+++ src/higher-order-func.js
@@ -17,6 +17,24 @@ const priceList = [10.5, 9.9, 8.9, 4.5];
 
 // Add code here
 
+function totalPrice() {
+  let totalPrice = 0;
+  for (let i = 0; i < priceList.length; i++) {
+    totalPrice += priceList[i];
+  }
+  console.log(`Total Price is ${totalPrice}`);
+
+  let payablePrice = 0;
+  function addGST() {
+    payablePrice = totalPrice * 1.07;
+    return payablePrice;
+  }
+  addGST();
+  console.log(`Payable Price is ${payablePrice}`);
+}
+
+totalPrice();
+
 /*
     Task 2. Manipulate the "queue" array.
 */
@@ -29,6 +47,12 @@ function createQueue(fn) {
 
 // Add code here - define the function that adds "start" and "last" to a given array
 
-const result = createQueue(); // Add code here - pass a reference of a function as an argument
+function addQueue(queue) {
+  queue.unshift("Start");
+  queue.push("Last");
+  return queue;
+}
+
+const result = createQueue(addQueue); // Add code here - pass a reference of a function as an argument
 
 console.log(result); // Expected output: ["start", 1, 2, 3, 4, "last"];
